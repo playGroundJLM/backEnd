888fe3c5d3b6ab9af4a568f86d30457da1460526
Erez Levanon <erez.levanon@mail.huji.ac.il>
2016-05-20 01:55:53 +0300

    elevanon: add read/write results options in ccreategraph

--- createGraph.py
+++ createGraph.py
@@ -1,35 +1,47 @@
 import xml.etree.ElementTree
+import sys
 from track import Track
 import track
-e = xml.etree.ElementTree.parse("./sampleTracks.kml").getroot()
+import pickle
 
-paths = []
+if __name__ == "__main__":
+    if sys.argv[1] == "write":
+        e = xml.etree.ElementTree.parse("./sampleTracks.kml").getroot()
 
-PREFIX = "{http://www.opengis.net/kml/2.2}"
+        paths = []
 
+        PREFIX = "{http://www.opengis.net/kml/2.2}"
 
+        for path in e.iter(PREFIX + "coordinates"):
+            curPath = []
+            for point in path.text.strip().split(",0.0")[0:-1]:
+                latLong = point.strip().split(",")
+                curPath.append((latLong[1], latLong[0]),)
+            paths.append(curPath)
 
-for path in e.iter(PREFIX + "coordinates"):
-    curPath = []
-    for point in path.text.strip().split(",0.0")[0:-1]:
-        latLong = point.strip().split(",")
-        curPath.append((latLong[1], latLong[0]),)
-    paths.append(curPath)
+        tracks = []
 
-tracks = []
+        for j, i in enumerate(paths):
+            print("----------------{0}-----------------".format(j))
+            t = Track(i)
+            print(t.dist)
+            print(t.maxIncline)
+            print(t.meanIncline)
+            tracks.append(t)
 
-for j, i in enumerate(paths):
-    print("----------------{0}-----------------".format(j))
-    t = Track(i)
-    print(t.dist)
-    print(t.maxIncline)
-    print(t.meanIncline)
-    tracks.append(t)
+        userPrefs = {"dist": 3.25, "facilities": True, "water": False, "incline": 3, "stairs": False}
 
-userPrefs = {"dist": 3.25, "facilities": True, "water": False, "incline": 1, "stairs": False}
+        res = track.closestTracks(tracks, userPrefs)
 
-res = track.closestTracks(tracks, userPrefs)
+        res = track.createJsonResponse(res)
+        print(res)
+        with open("./resPickle.pickle", 'wb') as f:
+            pickle.dump(res, f)
 
-res = track.createJsonResponse(res)
+    elif sys.argv[1] == "read":
+        with open("./resPickle.pickle", "rb") as f:
+            res = pickle.load(f)
+        print(res)
 
-print(res)
+    else:
+        print("wrong args")
